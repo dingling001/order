c7df209a517025301f42ecef5c27cb3ddcebdd57
dl <215594238@qq.com>
2019-03-21 07:14:19 +0800

    3.21

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,17 +1,61 @@
 // pages/login/login.js
+let network = require('../../utils/network.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    btnValue: '获取验证码',
+    btnDisabled: false,
+    phone: '',
+    code: '',
+    second: 60
   },
-
+  timer: function () {
+    let promise = new Promise((resolve, reject) => {
+      let setTimer = setInterval(
+        () => {
+          var second = this.data.second - 1;
+          this.setData({
+            second: second,
+            btnValue: second + '秒',
+            btnDisabled: true
+          })
+          if (this.data.second <= 0) {
+            this.setData({
+              second: 60,
+              btnValue: '获取验证码',
+              btnDisabled: false
+            })
+            resolve(setTimer)
+          }
+        }
+        , 1000)
+    })
+    promise.then((setTimer) => {
+      clearInterval(setTimer)
+    })
+  },
   onGotUserInfo(e) {
     wx.switchTab({
       url: '../index/index',
     })
+  },
+  phone_fun(e) {
+    this.setData({
+      phone: e.detail.value
+    })
+  },
+  // 发送验证码
+  getCode() {
+    let phoen_reg = /^[1][3,4,5,7,8][0-9]{9}$/;
+    this.timer()
+    // /wxclient/user / sendCode
+  },
+  // 登录
+  login_fun(e) {
+    // /wxclient/user / login
   },
   onLoad: function(options) {
 
