b89944ece96646145f41b33f108b6994102f7e30
dingling <dingling>
2019-03-30 18:49:46 +0800

    菜农庄3.30

--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -6,20 +6,52 @@ Page({
    * 页面的初始数据
    */
   data: {
-    palt: ['饿了么', '美团外卖', '京东到家'],
+    palt: [],
     palt_name: '线上平台菜单导入',
-    index: -1,
+    palt_list: [],
+    index: 0,
     goodsList: [],
     gategory: [],
     gindex: 0
   },
   palt_fun(e) {
-    console.log(e)
     this.setData({
       index: e.detail.value
     })
+    console.log(this.data.palt_list[e.detail.value])
+    this.importCategory(this.data.palt_list[e.detail.value].code)
+    this.getGoodsList(this.data.palt[e.detail.value], '')
   },
-  getGoodsList() {
+
+  get_platform() {
+    let that = this;
+    wx.getStorage({
+      key: 'token',
+      success: function(res_token) {
+        network.GET({
+          url: 'wxclient/platform/list',
+          header: {
+            "Content-Type": "application/json;charset=UTF-8",
+            "token": res_token.data
+          },
+          params: {
+            paltform: 'wx',
+          },
+          success(res) {
+            let palt = [];
+            for (let i in res.data.data) {
+              palt.push(res.data.data[i].name)
+            }
+            that.setData({
+              palt: palt,
+              palt_list: res.data.data
+            })
+          },
+        })
+      },
+    })
+  },
+  getGoodsList(name = '', secondaryCategory = '') {
     let that = this;
     wx.showLoading({
       title: '加载中……',
@@ -35,8 +67,8 @@ Page({
           },
           params: {
             paltform: 'wx',
-            name: '',
-            secondaryCategory: ''
+            name: name,
+            secondaryCategory: secondaryCategory
           },
           success(res) {
             let goodsList = res.data.data;
@@ -46,6 +78,7 @@ Page({
             that.setData({
               goodsList: res.data.data
             })
+
             wx.hideLoading()
           },
         })
@@ -53,7 +86,12 @@ Page({
     })
   },
   left_nav(e) {
-
+    console.log(e)
+    let gindex = e.currentTarget.dataset.index;
+    this.setData({
+      gindex: gindex
+    })
+    this.getGoodsList(this.data.gategory[gindex].name)
   },
   // 获取分类
   getCategoryVo() {
@@ -71,14 +109,43 @@ Page({
             paltform: 'wx',
           },
           success(res) {
-            console.log(res)
+            that.getGoodsList(res.data.data[0].name)
+            that.setData({
+              gategory: res.data.data
+            })
+          },
+        })
+      },
+    })
+  },
+  // 根据平台导入分类数据
+  importCategory(platformCode) {
+    let that = this;
+    wx.getStorage({
+      key: 'token',
+      success: function(res_token) {
+        network.GET({
+          url: 'wxclient/category/importCategory',
+          header: {
+            "Content-Type": "application/json;charset=UTF-8",
+            "token": res_token.data
+          },
+          params: {
+            paltform: 'wx',
+            platformCode: platformCode
+          },
+          success(res) {
+            that.getGoodsList(res.data.data[0].name)
+            that.setData({
+              gategory: res.data.data
+            })
           },
         })
       },
     })
   },
   onLoad: function(options) {
-    this.getGoodsList();
+    this.get_platform();
     this.getCategoryVo();
   },
 
